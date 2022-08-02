e69f53c6e4ff9c336f69df8393ccff145ee1b107
stephaff <affalistephane@gmail.com>
2022-08-02 10:22:19 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import { Component } from 'react';
+import './App.css';
+
+class App extends Component{
+
+  state = {
+    fullName : '',
+    bio : '',
+    imgSrc : '',
+    alt : '',
+    profession : ''
+  };
+  
+  show = false;
+
+  displayProfil = () =>{
+    if(this.show === false){
+      document.querySelector('button').innerHTML = 'Cacher le profil';
+      this.setState({
+      fullName : 'Stephane Affali',
+      bio : 'lorem vyyy jgugiug ihihiui',
+      imgSrc : 'photo.jpeg',
+      alt : 'myPhoto',
+      profession : 'Web developper'
+    })
+    this.show = true;
+    }
+    else{
+      document.querySelector('button').innerHTML = 'Afficher le profil';
+      this.setState({
+      fullName : '',
+      bio : '',
+      imgSrc : '',
+      alt : '',
+      profession : ''
+    })
+    this.show = false;
+    } 
+  }
+
+  render(){
+    return (
+      <>
+        <div className='person'>
+          <div className='person-right'>
+            <p>Fullname : {this.state.fullName}</p>
+            <p>Bio : {this.state.bio}</p>
+            <p>Profession : {this.state.profession}</p>
+          </div>
+          <img src={this.state.imgSrc} alt={this.state.alt} className='person-left' />
+        </div>
+        <div className='button-display-container'>
+          <button onClick={this.displayProfil}>Afficher profil</button>
+        </div>
+      </>
+    );
+  }
+}
+
+export default App;
